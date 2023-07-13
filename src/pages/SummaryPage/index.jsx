a64a250091ac7e586d3c5e7d74ef4c9f58d43274
Dorabang <psa1195@naver.com>
2023-07-13 19:33:46 +0900

    feat: 주문 페이지에서 선택한 주문 확인하기

--- src/pages/SummaryPage/index.jsx
+++ src/pages/SummaryPage/index.jsx
@@ -1,11 +1,44 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { OrderContext } from '../../context/OrderContext';
 
-const SummaryPage = () => {
+const SummaryPage = ({ setStep }) => {
+  const [orderDetails] = useContext(OrderContext);
   const [checked, setChecked] = useState(false);
+
+  const productArray = Array.from(orderDetails.products);
+  const productList = productArray.map(([key, value]) => (
+    <li key={key}>
+      {key}, {value}EA
+    </li>
+  ));
+
+  const hasOptions = orderDetails.options.size > 0;
+  let optionsDisplay = null;
+
+  if (hasOptions) {
+    const optionsArray = Array.from(orderDetails.options.keys());
+    const optionList = optionsArray.map((key) => <li key={key}>{key}</li>);
+    optionsDisplay = (
+      <>
+        <h3>Option : {orderDetails.totals.options}</h3>
+        <ul>{optionList}</ul>
+      </>
+    );
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setStep(2);
+  };
 
   return (
     <div>
-      <form>
+      <h2>주문 확인</h2>
+      <h3>Products : {orderDetails.totals.products}</h3>
+      <ul>{productList}</ul>
+      {optionsDisplay}
+
+      <form onSubmit={handleSubmit}>
         <input
           type='checkbox'
           checked={checked}
